33e18f5d236b0e6604dfab25639bdb49ba149690
charlesdev96 <ogonnayacn@gmail.com>
2024-05-15 20:04:43 +0100

    comment

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,3 @@
-// import globals from "globals";
-// import tseslint from "typescript-eslint";
-
-// export default [
-// 	{ files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-// 	{ languageOptions: { globals: globals.browser } },
-// 	overrides: [
-// 		{
-// 			files: ["**/*.ts"],
-// 			rules: {
-// 				"@typescript-eslint/no-explicit-any": "off",
-// 				"@typescript-eslint/ban-types": "off",
-// 				"@typescript-eslint/no-unused-vars": "off",
-// 			},
-// 		},
-// 	],
-// 	...tseslint.configs.recommended,
-// ];
 export default {
 	overrides: [
 		{
